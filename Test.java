6f550a133522e3068c1141c700537eecea285d80
WangGuanqian <wang_guanqian@126.com>
2018-10-27 17:40:46 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,8 +3,8 @@ public class Test {
 	public static void main(String[] args){
 		System.out.println("测试。。。");
 
-		int a = 1;
-		int b = 2;
+		int a = 3;
+		int b = 4;
 		System.out.println("a+b=" a+b);
 		System.out.println("a+b=" a+b);
 	}
